e80ec35f3249458034946fda4a74d7859f8bcded
Saad Masood <saadmasood@web.de>
2018-03-24 21:50:27 +0100

    moving methods in Class Player

--- core/src/de/opencodes/boxhunter/Player.java
+++ core/src/de/opencodes/boxhunter/Player.java
@@ -20,28 +20,59 @@ public class Player {
 		position = new Vector2(xPos, yPos);
 	}
 
-	// ------------------------getter and setter-----------------------------
-	public Rectangle getRectangle() {
-		return myRectangle;
-	}
 
-	public void setRectangle(Vector2 position, int width, int height) {
-		this.position = position;
-		this.width = width;
-		this.heigth = height;
-	}
 
-	public Vector2 getPosition() {
-		return position;
-	}
 
-	public void setPosition(Vector2 position) {
-		this.position = position;
-	}
+    //------------------------------methods----------------------------------
 
-	public int getWidth() {
-		return width;
-	}
+
+    public void moveLeft() {
+        //todo change dummy speed
+        velocity.x = -5;
+        //todo check if there is obstacle on left side?
+
+        //check if there is the map wall on left side??
+            movePosition();
+        velocity.x = 0;
+    }
+    public void moveRight() {
+        //todo chaneg dummy speed
+        velocity.x = 5;
+        //todo check if there is obstacle on right side?
+        //check if there is map wall right on side`???
+        movePosition();
+
+        velocity.x = 0;
+    }
+    public void moveUp() {
+        velocity.y = 5;
+        movePosition();
+    }
+    public void moveDown() {
+        velocity.y = -5;
+        movePosition();
+
+    }
+
+    public void movePosition() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    }
+
+
+
+
+    //------------------------getter and setter-----------------------------
+    public Rectangle getRectangle() {
+        return myRectangle;
+    }
+
+    public void setRectangle(Vector2 position, int width, int height) {
+        this.position = position;
+        this.width = width;
+        this.heigth = height;
+    }
+
 
 	public void setWidth(int width) {
 		this.width = width;
@@ -55,12 +86,30 @@ public class Player {
 		this.heigth = heigth;
 	}
 
+
 	public Vector2 getVelocity() {
 		return velocity;
 	}
 
+
+
+    public int getWidth() {
+        return width;
+    }
+
+
 	public void setVelocity(Vector2 velocity) {
 		this.velocity = velocity;
 	}
+
+
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
 
 }
